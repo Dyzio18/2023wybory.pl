4fd5fa7803200aae1f969d99465d14064af8d488
Dyzio18 <dyzio18@gmail.com>
2023-09-12 19:29:28 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
 import { Footer } from '@/components/Footer/Footer';
@@ -38,6 +39,7 @@ export default function RootLayout({
           {children}
         </div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
